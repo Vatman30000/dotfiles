d0da7db29a864161597018029ce6d40297874c93
Александр <a.matyasov0@gmail.com>
2026-08-08 22:59:11 +0300

    добавил ssh копирование в tmux и в remote nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,3 +5,7 @@
 -- vim.opt.winblend = 15 -- Прозрачность плавающих окон (0-100)
 -- vim.opt.pumblend = 15 -- Прозрачность выпадающих меню (0-100)
 -- vim.opt.background = "light" -- или 'dark', может влиять на видимость
+if vim.env.SSH_TTY and not vim.g.remote_neovim_host then
+  vim.g.clipboard = "osc52"
+  vim.opt.clipboard = "unnamedplus"
+end
